1117c17bfb215dceca643ccb330437a4dc22d25d
dangha333 <146915988+dangha333@users.noreply.github.com>
2025-03-25 13:57:14 +0700

    ha

---
+++ app/Models/Product_comment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product_comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductCommentFactory> */
+    use HasFactory;
+    protected $table = 'product_comments';
+}
